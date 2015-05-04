a5cc639a272e469a3909b9a3af128000e0b95fe5
Thiago Kairala <thiagor.kairala@gmail.com>
2015-05-04 14:46:50 -0300

    Using orientation change

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,11 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-var app = angular.module('adivinha', ['ionic', 'adivinha.controllers', 'adivinha.services'])
+var app = angular.module('adivinha', [
+  'ionic',
+  'adivinha.controllers',
+  'adivinha.services'
+  ])
 
 app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -38,11 +42,20 @@ app.config(function($stateProvider, $urlRouterProvider) {
     }
   })
   .state('game.match', {
-    url: '/match',
+    url: '/match/:typeName/:backgroundColor',
     views: {
       'match': {
         templateUrl: 'templates/match.html',
         controller: 'MatchCtrl'
+      }
+    }
+  })
+  .state('game.result', {
+    url: '/result',
+    views: {
+      'result': {
+        templateUrl: 'templates/result.html',
+        controller: 'ResultCtrl'
       }
     }
   });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -6,6 +6,27 @@ controller.controller('BeginCtrl', function($scope, QuestionsTypes) {
 	});
 });
 
-controller.controller('MatchCtrl', function($scope, $stateParams, QuestionsTypes) {
+controller.controller('MatchCtrl', function($scope, $stateParams, $timeout, $state) {
+	screen.lockOrientation('landscape');
+
+	$scope.questionType = $stateParams.typeName;
+	$scope.backGroundColor = $stateParams.backgroundColor;
 	
+	// preparing counter
+	$scope.counter = 20;
+	$scope.countDown = function() {
+		$scope.counter--;
+		if($scope.counter == 0) {
+			$state.go('game.result');
+		}
+		$timeout($scope.countDown, 1000);
+	}
+
+
+	// starting counter
+	var start = $timeout($scope.countDown, 1000);
+});
+
+controller.controller('ResultCtrl', function($scope) {
+
 });
